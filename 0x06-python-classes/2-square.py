a8879d43ef915e1797a3e5b767c4fdb84cf16a74
1979dany <danymail1979@gmail.com>
2022-09-13 15:19:48 -0700

    A class Square that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""Example Google style docstrings.
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
+"""A class Square that defines a square
+by: (based on 1-square.py)
+
 """
 
 
 class Square:
-    """ A class that defines a square by its size
-    This defines an empty square class.
-    As instructed, no method or attribute is created
+    """Square class with a private attribute
+    size.
+
     """
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        This defines an empty square class.
-        As instructed, no method or attribute is created
+        """Initializes the size variable as a private
+        instance artribute
+
+        Args:
+            __size (int): The __size of the new square.
+
         """
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size) 
